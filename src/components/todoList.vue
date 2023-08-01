1637d40f02ecc1306b364100e0bfa7d1e86772c9
ibrahimmjs <imaras3838@gmail.com>
2023-08-01 16:49:46 +0300

    new css

--- src/components/todoList.vue
+++ src/components/todoList.vue
@@ -1,13 +1,19 @@
 <template>
-    <input type="text" id="todoText" v-model="todoText">
-        <button id="addBtn" @click="addTodo">Ekle</button>
-        <button id="deleteBtn" @click="deleteTodo">Sil</button>
-        <ul type="none" id="todoList">
-          <li v-for="todo in todoList" :key="todo"> {{ todo }} </li>
-        </ul>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
+
+<div class="mainDiv">
+    <input type="text" id="todoText" placeholder="Eklemek istediğiniz ürünü girin" v-model="todoText">
+    <button id="addBtn" @click="addTodo">Ekle</button>
+    <button id="deleteBtn" @click="deleteTodo">Sil</button>
+    <ul type="none" id="todoList">
+      <li @click="liClick" v-for="todo in todoList" :key="todo"> {{ todo }} </li>
+    </ul>
+  </div>
     </template>
     
-    <script>
+    <script>    
     export default {
       data() {
         return {
@@ -23,12 +29,14 @@
         deleteTodo(){
           this.todoList.pop(this.todoText)
         },
+        liClick(){
+        let todoSelect = document.querySelector("#todoList")
+        if(todoSelect.classList.contains("lineThrough")){
+          todoSelect.classList.remove("lineThrough")
+          return;
+        }
+        todoSelect.classList.add("lineThrough")
+      }
       },
     }
     </script>
-    
-    <style>
-    
-    
-    
-    </style>
